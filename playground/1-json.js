45611b354dddf89b9823f6d57ef22dcccd11e671
“[Rishabh <[rshrest3@go.olemiss.edu]>
2021-06-29 23:19:34 -0500

    JSON new

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,21 @@
-const book={
-    title:'Ego is the enemy',
-    author:'Ryan Holiday'
-}
+const fs=require('fs')
 
-const bookJSON=JSON.stringify(book)
-console.log(bookJSON)
+//const book={
+//    title:'Ego is the enemy',
+//    author:'Ryan Holiday'
+//}
+
+//const bookJSON=JSON.stringify(book)
+//fs.writeFileSync('1-json.json',bookJSON)
+
+const dataBuffer=fs.readFileSync('1-json.json') 
+const dataJSON=dataBuffer.toString()
+console.log(dataJSON)
+const data=JSON.parse(dataJSON)
+
+data.name="Rishabh"
+data.age=22
+
+console.log(data)
+newJSON=JSON.stringify(data)
+fs.writeFileSync('1-json.json',newJSON)
