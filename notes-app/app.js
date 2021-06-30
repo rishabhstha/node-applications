85f7b8a0d5c4481739b7243453cfd7899ca96f2a
“[Rishabh <[rshrest3@go.olemiss.edu]>
2021-06-29 22:34:34 -0500

    first commit

---
+++ notes-app/app.js
@@ -0,0 +1,96 @@
+const chalk= require('chalk')
+const yargs=require('yargs')
+
+const getNotes=require('./notes.js')
+
+//Customize yargs version
+yargs.version('1.1.0')
+
+//add, remove, read, list
+
+//Create add command
+yargs.command({
+    command:'add',
+    describe:'Adding a note',
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Note body',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(argv) {
+        console.log('Title: '+argv.title)
+        console.log('Body: '+argv.body)
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Removing a note',
+    handler:function(){
+        console.log('Removing the note')
+    }
+})
+ 
+yargs.command({
+    command:'read',
+    describe:'Reading a note',
+    handler:function(){
+        console.log('Reading the note')
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'Listing your notes',
+    handler:function(){
+        console.log('Listing all notes')
+    }
+})
+
+yargs.parse()
+
+//console.log(yargs.argv)
+
+
+
+
+
+// const chalk=require('chalk');
+// console.log(chalk.green('Success!!'))
+// const error=chalk.red.inverse.bold;
+// const warning=chalk.keyword('orange')
+// console.log(error('Error'));
+// console.log(warning('Warning!'))
+
+// const myNotes=require('./notes.js')
+// const msg=myNotes()
+// console.log(msg)
+
+// console.log(process.argv[2])
+
+
+
+// const validator=require('validator')
+
+// console.log(validator.isEmail('rishabhstha@gmail.com'))
+// console.log(validator.isURL('https/mead.io'))
+
+// const fs=require('fs')
+// fs.writeFileSync('notes.txt','My name is Rishabh.')
+// fs.appendFileSync('notes.txt',' I am 22 years old.')
+
+// const add= require('./utils.js');
+// const sum=add(4,-2)
+// console.log(sum)
+
+//Challenge
+// const myNotes=require('./notes.js')
+// const msg=myNotes()
+// console.log(msg)
