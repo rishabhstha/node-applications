ce3805a2b196c925a8a75adad58e75a5a54eda76
“[Rishabh <rishabhstha@gmail.com>
2021-07-09 16:19:06 -0500

    command line argument input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,11 @@
 const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
-
-geocode('Oxford MS',(error, data)=>{
+//console.log(process.argv[2])
+geocode(process.argv[2],(error, data)=>{
     if(error){
         return console.log('Error', error)
     }
-
+  
     forecast(data.latitude, data.longitude, (error, forecastData) => {
         if(error){
             return console.log('Error', error)
