337cadd5ba86c590b5ba197a527f6b7e24adc319
“[Rishabh <rishabhstha@gmail.com>
2021-07-10 18:47:15 -0500

    es6 changes

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,12 @@
+//Object property shorthand
+
+const name='Andrew'
+const userAge=22
+
+const user={
+    name,
+    age: userAge,
+    location:'Oxford'
+}
+
+console.log(user)
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,25 @@
 const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
-//console.log(process.argv[2])
-geocode(process.argv[2],(error, data)=>{
-    if(error){
-        return console.log('Error', error)
-    }
-  
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+
+const address=process.argv[2]
+
+if(!address){
+    console.log("Please provide a address!")
+}else{
+
+    geocode(address,(error, data)=>{
         if(error){
             return console.log('Error', error)
         }
-        console.log(data.location)
-        console.log(forecastData)
-      })
-})
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error){
+                return console.log('Error', error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
 
 
