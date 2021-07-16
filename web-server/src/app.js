7a11a1e6e8785decc05ab3f11d61cb6fabcf30f0
“[Rishabh <rishabhstha@gmail.com>
2021-07-16 12:33:31 -0500

    customizing views and advanced templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,20 @@
 const path=require('path')
-
 const express=require('express')
+const hbs=require('hbs')
 
-//console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
 const app=express()
-const publicDirectoryPath=path.join(__dirname, '../public')
 
+//Define paths for Express config
+const publicDirectoryPath=path.join(__dirname, '../public')
+const viewsPath=path.join(__dirname,'../templates/views')
+const partialsPath=path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine and views location
 app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res)=>{
@@ -26,7 +33,34 @@ app.get('/about',(req,res)=>{
 
 app.get('/help',(req,res)=>{
     res.render('help',{
-        helpText:"This is some helpful message"
+        helpText:"This is some helpful message",
+        title:'Help',
+        name:'Rishabh Shrestha'
+
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    res.send({
+        location:"Philadelphia",
+        forecast:"It is sunny out there"
+    })
+})
+
+app.get('/help/*',(req, res)=>{
+    res.render('error',{
+        title:"Error 404",
+        errorText:"Help article not found",
+        name:"Rishabh Shrestha"
+    })
+    
+})
+
+app.get('*',(req, res)=>{
+    res.render('error',{
+        title:"Error 404",
+        errorText:"Page not found",
+        name:"Rishabh Shrestha"
     })
 })
 
@@ -58,12 +92,7 @@ app.get('/help',(req,res)=>{
 //     res.sendFile(path.join(__dirname, '../public/help.html'))
 // })
 
-app.get('/weather',(req,res)=>{
-    res.send({
-        location:"Philadelphia",
-        forecast:"It is sunny out there"
-    })
-})
+
 //app.com
 app.listen(3000,()=>{
     console.log("Server is up on port 3000.")
