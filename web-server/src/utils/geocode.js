aab4bbaa310498115705753517cfba403a9c912f
“[Rishabh <rishabhstha@gmail.com>
2021-07-20 21:26:27 -0500

    Browser http requests with fetch

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -4,18 +4,18 @@ const geocode=(address, callback)=>{
     const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoicmlzaGFiaHN0aGEiLCJhIjoiY2txcmE2c2x6MXA4czJ1bzh3aDZqc3UwZSJ9.f_hBTLICQKOvpfNmw8qYIg&limit=1'
     request({url, json:true},(error, {body}={})=>{ //destructuring resposne to body
       //  const {body}= response
-        if(error){
+       if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if(body.features===undefined){
-            callback('Unable to find location. Try another search')
-        }else {
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
             callback(undefined, {
                 latitude: body.features[0].center[1],
-                longitude:body.features[0].center[0],
+                longitude: body.features[0].center[0],
                 location: body.features[0].place_name
             })
         }
     })
 }
 
-module.exports=geocode
+module.exports = geocode
