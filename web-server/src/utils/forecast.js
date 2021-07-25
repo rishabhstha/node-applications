52e96bae403b49097788ac038d144932d23e21c9
“[Rishabh <rishabhstha@gmail.com>
2021-07-25 11:56:15 -0500

    updated package.json, deployed to heroku, and change to forecast.js

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast=(lat,long,callback)=>{
             callback('Unable to find the location!', undefined)
         }else{
             callback(undefined,               
-                body.current.weather_descriptions[0]+`. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+                body.current.weather_descriptions[0]+`. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidiy is ${body.current.humidity}%`
             )
         }
     })
