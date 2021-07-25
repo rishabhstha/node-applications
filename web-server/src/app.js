634b5a251fbe8ba85bdfc7b99a5508e36174ab42
“[Rishabh <rishabhstha@gmail.com>
2021-07-25 00:58:00 -0500

    fixed to correct variable name

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,10 +6,11 @@ const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast') 
 
 const app=express()
+const port= process.env.PORT || 3000
 
-//Define paths for Express config
+//Define paths for Express configuration
 const publicDirectoryPath=path.join(__dirname, '../public')
-const viewsPath=path.join(__dirname,'../templates/views')``
+const viewsPath=path.join(__dirname,'../templates/views')
 const partialsPath=path.join(__dirname,'../templates/partials')
 
 //Setup handlebars engine and views location
@@ -128,6 +129,6 @@ app.get('*',(req, res)=>{
 
 
 //app.com
-app.listen(3000,()=>{
-    console.log("Server is up on port 3000.")
+app.listen(port,()=>{
+    console.log("Server is up on port "+ port)
 })
