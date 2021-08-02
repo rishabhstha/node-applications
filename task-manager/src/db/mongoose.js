37dfc20b427418f3632fbf9f2af2a937c9e31ddc
“[Rishabh <rishabhstha@gmail.com>
2021-08-01 22:25:45 -0500

    Adding task model using mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose= require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true 
 })
 
 const User=mongoose.model('User', {
@@ -14,13 +15,36 @@ const User=mongoose.model('User', {
     }
 })
 
-const me= new User({
-    name: 'Rishabh',
-    age: 'Yes'
+const Task=mongoose.model('Task',{
+    description: {
+        type:String
+    },
+    completed:{
+        type: Boolean
+    }
+
 })
 
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log('Error', error)
+// Creating an instance of Task model
+const read= new Task({
+    description: "Read",
+    completed: false
 })
+
+read.save().then(()=>{
+    console.log(read)
+}).catch((error)=>{
+    console.log(error)
+})
+
+//Creating a model for User
+// const me= new User({
+//     name: 'Rishabh',
+//     age: 'Yes'
+// })
+
+// me.save().then(()=>{
+//     console.log(me)
+// }).catch((error)=>{
+//     console.log('Error', error)
+// })
