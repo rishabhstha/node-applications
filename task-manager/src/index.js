3bc94d420b288cd0431adb91a0fc7ad1f93217bf
“[Rishabh <rishabhstha@gmail.com>
2021-08-18 23:19:46 -0500

    Added resource update endpoint for tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -91,7 +91,7 @@ app.patch('/users/:id', async(req, res)=>{
     }
     try{
         const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-
+  
         if(!user) {
             return res.status(404).send()
         }
@@ -164,6 +164,32 @@ app.get('/tasks/:id',async (req,res)=>{
     //     res.status(500).send(e)
     // })
 })
+
+//Create an endpoint to allow for task updates
+app.patch('/tasks/:id',async(req,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates!'})
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+  
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e){
+         if(e.name === 'CastError'){
+        return res.status(404).send('Invalid id')
+    }
+         res.status(500).send(e)
+    }
+})
+
 
 
 app.listen(port, ()=>{
