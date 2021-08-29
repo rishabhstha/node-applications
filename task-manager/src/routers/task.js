10702bc063052add7b3858d14df53e18299bf37e
“[Rishabh <rishabhstha@gmail.com>
2021-08-29 16:34:56 -0500

    Updating task by id and owner

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -31,11 +31,11 @@ router.post('/tasks', auth, async (req, res)=>{
 })
 
 // Create an endpoint for fetching all tasks
-router.get('/tasks', async(req,res)=>{
+router.get('/tasks', auth, async(req,res)=>{
     console.log("aye")
 
     try{
-        const tasks= await Task.find({})
+        const tasks= await Task.find({owner:req.user._id})
         res.send(tasks)
     } catch(e){
         res.status(500).send(e)
@@ -48,15 +48,18 @@ router.get('/tasks', async(req,res)=>{
 })
 
 //Create an endpoint for fetching task by its id
-router.get('/tasks/:id',async (req,res)=>{
+router.get('/tasks/:id',auth, async (req,res)=>{
     console.log("aye")
     const _id = req.params.id
     console.log(_id)
     try{
-        const task= await Task.findById(_id)
+        // const task= await Task.findById(_id)
+        const task = await Task.findOne({_id, owner: req.user._id})
+
         if(!task){
             return res.status(404).send()
         }
+
         res.send(task)
     } catch(e){
         res.status(500).send()
@@ -73,7 +76,7 @@ router.get('/tasks/:id',async (req,res)=>{
 })
 
 //Create an endpoint to allow for task updates
-router.patch('/tasks/:id',async(req,res)=>{
+router.patch('/tasks/:id',auth, async(req,res)=>{
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
@@ -83,16 +86,20 @@ router.patch('/tasks/:id',async(req,res)=>{
     }
 
     try{
-        const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({_id:req.params.id, owner: req.user._id})
+        // const task = await Task.findById(req.params.id)
+
+        if(!task) {
+            return res.status(404).send("Error: Task not found")
+        }
+
         updates.forEach((update)=>{
             task[update]= req.body[update]
         })
         await task.save()
         // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
   
-        if(!task) {
-            return res.status(404).send("Error: Task not found")
-        }
+        
         res.send(task)
     } catch (e){
          if(e.name === 'CastError'){
