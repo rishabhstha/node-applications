f2722a77817034f356a18def55408cad81ab5514
“[Rishabh <rishabhstha@gmail.com>
2021-08-29 20:57:10 -0500

    Deleting tasks along with the deleted user

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -3,6 +3,7 @@ const mongoose= require('mongoose')
 const validator= require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task= require('./task')
 
 const userSchema =  new mongoose.Schema({
     name: {
@@ -106,6 +107,14 @@ userSchema.pre('save', async function(next){
     // console.log('just before saving!')
     next()
 })
+
+//Delete user tasks when user is removed
+userSchema.pre('remove', async function(next){
+    const user =this
+    await Task.deleteMany({owner: user._id})
+    next()
+})
+
 
 // Creating a model for User
 const User=mongoose.model('User', userSchema)
