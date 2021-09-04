d41b393a515c6fed490336a4b7195971bb3c08d8
“[Rishabh <rishabhstha@gmail.com>
2021-09-03 20:02:26 -0500

    Validate profile picture upload with certain criteria

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -165,7 +165,16 @@ router.delete('/users/me', auth, async(req, res)=>{
 })
 
 const upload = multer({
-    dest: 'avatar'
+    dest: 'avatar',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload a jpg/jpeg/png file!'))
+        }
+        cb(undefined, true)
+    }
 })
 //route for uploading user's profile picture
 router.post('/users/me/avatar', upload.single('avatar'), (req, res)=>{
