9099be1a5a67d0797b14c7b72f6965ae1c9ec7cc
“[Rishabh <rishabhstha@gmail.com>
2021-09-05 20:34:56 -0500

    Adding images to user profile and deleting

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -165,7 +165,6 @@ router.delete('/users/me', auth, async(req, res)=>{
 })
 
 const upload = multer({
-    dest: 'avatar',
     limits: {
         fileSize: 1000000
     },
@@ -177,14 +176,24 @@ const upload = multer({
     }
 })
 
-
-
-
 //route for uploading user's profile picture
-router.post('/users/me/avatar', upload.single('avatar'), (req, res)=>{
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res)=>{
+    req.user.avatar= req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next)=>{
     res.status(400).send({error: error.message})
+})
+
+//route for deleting avatar
+router.delete('/users/me/avatar',auth, async (req, res)=>{
+    try{
+        req.user.avatar=undefined
+        await req.user.save()
+        res.send()
+    } catch(e){
+        res.status(500).send(e)
+    }
 })
 
 module.exports=router
