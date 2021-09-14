8c303aa321969a9b4a1796a9693a30ec8ed52a58
“[Rishabh <rishabhstha@gmail.com>
2021-09-13 23:53:32 -0500

    Creating environment variables

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,9 +1,7 @@
 const sgMail = require('@sendgrid/mail')
-const sendgridAPIKey = "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"
-//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
 
 
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name)=>{
     sgMail.send({
